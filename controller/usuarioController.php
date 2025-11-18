0908241b71ae39b6e1717dafab576dc37528b3db
Edwinson <Camix24k@gmail.com>
2025-11-18 15:32:15 -0500

    cambios db

---
+++ controller/usuarioController.php
@@ -0,0 +1,16 @@
+<?php
+// controller/usuarioController.php
+
+// SELECT 
+//     u.Id_Usuario,
+//     u.Nombre_Usuario,
+//     u.Password_Usuario,
+//     e.Id_Empleado,
+//     e.Correo_Electronico,
+//     e.Nombre_Empleado,
+//     e.Apellido_Empleado,
+//     u.Id_Rol
+// FROM tbl_usuario u
+// INNER JOIN tbl_empleado e ON u.Id_Empleado = e.Id_Empleado
+// WHERE u.Nombre_Usuario = :usuario
+// LIMIT 1;
